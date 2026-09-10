7c2e472152714ede2263e4cb98d5b71a48e0f916
sarasjbarbosa-hub <sarasjbarbosa@gmail.com>
2026-09-10 11:38:11 -0300

    criando rotas e inserindo pasta img e models

--- sprint-angular-frontend/src/app/pages/dashboard/dashboard.ts
+++ sprint-angular-frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Menu } from "../../componentes/menu/menu";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Menu],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
