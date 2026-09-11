7ea7e07b67a78d49772bac13781ce6e83cd324cd
sarasjbarbosa-hub <sarasjbarbosa@gmail.com>
2026-09-11 09:27:16 -0300

    adiciona lógica de redirecionamento no formulário de login e atualiza rotas para incluir dashboard

--- sprint-angular-frontend/src/app/app.routes.ts
+++ sprint-angular-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'home', component: Home }
+  { path: 'home', component: Home },
+  { path: 'dashboard', component: Dashboard }
 ];
